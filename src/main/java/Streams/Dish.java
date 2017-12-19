5a298fa15d4637139e8b73dd95886e494efd6e9b
arun789 <adwiti1975@gmail.com>
2017-12-19 00:54:15 -0700

    Streams

--- src/main/java/Streams/Dish.java
+++ src/main/java/Streams/Dish.java
@@ -1,4 +1,27 @@
 package Streams;
 
 public class Dish {
+    private String name;
+    private Integer calorie;
+
+    public Dish(String name, Integer calorie) {
+        this.name = name;
+        this.calorie = calorie;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getCalorie() {
+        return calorie;
+    }
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "name='" + name + '\'' +
+                ", calorie=" + calorie +
+                '}';
+    }
 }
