5e9e2c4c6a9b66585ab5a918fbf498ec9022ac37
arun789 <adwiti1975@gmail.com>
2018-01-12 00:02:19 -0700

    Function Interface of functional Interface

---
+++ src/main/java/FunctionalInterface/FunctionInterface.java
@@ -0,0 +1,26 @@
+package FunctionalInterface;
+
+import java.util.function.Function;
+
+/**
+ * FunctionInterface is a functional Interface which takes one parameter as input
+ * of any type and can return any type
+ */
+public class FunctionInterface {
+
+    public static void main(String[] args) {
+        /**
+         * Function interface which has got one abstract method apply(T t)
+         */
+        Function<String, Integer> f1 = s -> s.length();
+        Integer length = f1.apply("Arun");
+        System.out.println(length);
+
+        /**
+         * To return square of a number
+         */
+        Function<Integer, Integer> square = i -> i * i;
+        System.out.println(square.apply(10));
+    }
+
+}
