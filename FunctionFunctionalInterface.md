64719cb258847fa269dc3424a08c39c9c7e88030
arun789 <adwiti1975@gmail.com>
2018-01-12 23:59:32 -0700

    Function Interface with compose and andThen

--- FunctionFunctionalInterface.md
+++ FunctionFunctionalInterface.md
@@ -27,4 +27,23 @@ type and return type will be of any type.
         }
     
     }
- 
+    
+    
+    /**
+     * Difference between compose and andThen
+     */
+
+    /**
+     * First addAnumber will be executed that is 25 +5
+     * then it will be squared
+     * which means step 1 : 25 +5 = 30 and then 30 will be squared. 900
+     */
+    int andThenResult = addANumber.andThen(squareANumber).apply(25);
+    System.out.println(andThenResult);
+
+    /**
+     * first square a number will be executed 25 * 25 = 625
+     * and then 5 will be added to the result = 625 + 5 = 630
+     */
+    int composeResult = addANumber.compose(squareANumber).apply(25);
+    System.out.println(composeResult);
--- src/main/java/FunctionalInterface/FunctionInterface.java
+++ src/main/java/FunctionalInterface/FunctionInterface.java
@@ -20,7 +20,41 @@ public class FunctionInterface {
          * To return square of a number
          */
         Function<Integer, Integer> square = i -> i * i;
-        System.out.println(square.apply(10));
+        System.out.println(square.apply(10)); //o/p will be 100
+
+        /**
+         * Use Of Function Functional Interface to join more condition
+         * Requirement is
+         *
+         * When : Pass A Number
+         * Then : Add 5 to a number
+         * andThen : square the number
+         */
+
+        Function<Integer, Integer> addANumber = number -> number + 5;
+        Function<Integer, Integer> squareANumber = number -> number * number;
+
+        int result = addANumber.andThen(squareANumber).apply(5);
+        System.out.println(result); //o/p will be 100
+
+        /**
+         * Difference between compose and andThen
+         */
+
+        /**
+         * First addAnumber will be executed that is 25 +5
+         * then it will be squared
+         * which means step 1 : 25 +5 = 30 and then 30 will be squared. 900
+         */
+        int andThenResult = addANumber.andThen(squareANumber).apply(25);
+        System.out.println(andThenResult);
+
+        /**
+         * first square a number will be executed 25 * 25 = 625
+         * and then 5 will be added to the result = 625 + 5 = 630
+         */
+        int composeResult = addANumber.compose(squareANumber).apply(25);
+        System.out.println(composeResult);
     }
 
 }
