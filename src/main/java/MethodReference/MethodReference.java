177064aa07ec32dbb3f553e6bf12677b3a0e7a94
arun789 <adwiti1975@gmail.com>
2018-01-13 00:42:16 -0700

    Method Reference

---
+++ src/main/java/MethodReference/MethodReference.java
@@ -0,0 +1,35 @@
+package MethodReference;
+
+/**
+ * :: is used for Method reference, the argument type should be same and
+ * number of arguments should be same
+ */
+public class MethodReference {
+
+    public static void main(String[] args) {
+
+        interf i = MethodReference::m2; //Method Reference
+        i.m1();
+
+        interf3 i3 = new MethodReference()::m3;
+        String str = i3.m2();
+        System.out.println(str);
+    }
+
+    public static void m2() {
+        System.out.println("Method Reference of m2...");
+    }
+
+    public String m3() {
+        System.out.println("Instance Method m3...");
+        return "Method Reference returning String";
+    }
+}
+
+interface interf {
+    void m1();
+}
+
+interface interf3 {
+    String m2();
+}
